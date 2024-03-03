bfa0003291162fd83e0c3c20ae939c568268cf9c
Mathilde <kretz.mathilde@icloud.com>
2024-03-03 11:02:38 +0100

    lots of prints for debugging

--- Coach.py
+++ Coach.py
@@ -66,9 +66,13 @@ class Coach():
                 trainExamples.append([b, self.curPlayer, p, None])
 
             action = np.random.choice(len(pi), p=pi)
+            # print('caoch, le board', canonicalBoard)
+            # print("coach, l'action", action)
+            # print('coach, la ppbty', pi)
             board, self.curPlayer = self.game.getNextState(board, self.curPlayer, action)
 
             r = self.game.getGameEnded(board, self.curPlayer)
+            # print('coach, le end game de board:', r)
 
             if r != 0:
                 return [(x[0], x[2], r * ((-1) ** (x[1] != self.curPlayer))) for x in trainExamples]
